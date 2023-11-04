4a0573de9b369cd40020f3f818588b49eed8598c
emappstore777 <emappstore777@gmail.com>
2023-11-05 02:51:46 +0400

    added some funcs / gets

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ class Task {
     #cost;
 
     constructor (id, description, cost) {
-        this.#id = id;
-        id = "id" + Math.random().toString(16).slice(2);
+        this.#id = id
         this.#description = description;
         this.#cost = cost;
     }
@@ -26,15 +25,81 @@ class Task {
 
 }
 
-function IncomeTask() {
+function IncomeTask(id, description, cost) {
+
+    const _task = new Task (id, description, cost);
+
+    this.makeDone = {
+
+    }
+
+    this.makeUnDone = {
+
+    }
 
 }
 
-function ExpenseTask() {
+function ExpenseTask(id, description, cost) {
+
+    const _task = new Task (id, description, cost);
+
+    this.makeDone = {
+
+    }
+
+    this.makeUnDone = {
+
+    }
+
 
 }
 
 class TasksController {
+
+    #tasks;
+
+    constructor(tasks) {
+
+        this.#tasks = taskList;
+
+        Object.defineProperty(this, 'taskList', {
+            get() {
+                return taskList;
+            }
+        })
+        
+        this.addTasks = function (...taskList) {
+            taskList.push(...taskList);
+        }
+        
+        this.delete = function(taskList) {
+            const indexOfTaskList = taskList.indexOf(taskList);
+
+            if (indexOfTaskList !== -1) {
+                taskList.splice(indexOfTaskList, 1);
+            }
+        }
+
+        this.getTasksSortedBy = function() {
+            return taskList.sort(function(a, b) {
+                if (a > b) return 1;
+                if (a < b) return -1;
+    
+                return 0;
+            });
+        }
+
+        this.getFilteredTasks = function(inMainTeam) {
+            const filteredFootballPlayers = [];
+    
+            for(const footballPlayer of _footballPlayers) {
+                if (footballPlayer === inMainTeam) {
+                    filteredFootballPlayers.push(footballPlayer);
+                }
+            }
+    }
+
+    taskList = [];
     
 }
 
